1369d2a19fafe61e505adb1da09ac7a9917a0fc7
gaspar <contatomatheusgaspar@gmail.com>
2024-05-02 13:00:04 -0300

    update: news

--- diretivas/src/App.vue
+++ diretivas/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
-    <h2 v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaractetes: 15 }">teste de priquito seco e azedo amargo cuscuz</h2>
-    <h4 v-texto="{ cor: 'lightpink', tamanhoFonte: '300%'}">Arroz</h4>
+    <p>Sem diretiva</p>
+    <h2 v-texto="{ cor: 'red', tamanhoFonte: '200%', totalCaracteres: 15 }">teste de priquito seco e azedo amargo cuscuz</h2>
+    <h4 v-texto="configuracaoTexto">Arroz</h4>
+    <div v-posicao:absolute="75" v-texto="{ cor: 'green' }">Pega o doido do binding de argumento e valor</div>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
+  data: () => ({
+    configuracaoTexto: { cor: 'lightpink', tamanhoFonte: '300%', totalCaracteres: 5}
+  })
 }
 </script>
 
--- diretivas/src/main.js
+++ diretivas/src/main.js
@@ -4,13 +4,13 @@ import App from './App.vue'
 const Vue = createApp(App)
 
 Vue.directive('texto', {
-    created(el, biding){
+    created(el, binding){
 
-    if(biding.value?.cor) el.style.color = biding.value.cor
-    if(biding.value?.tamanhofonte) el.style.fontSize = biding.value.tamanhofonte
+    if(binding.value?.cor) el.style.color = binding.value.cor
+    if(binding.value?.tamanhofonte) el.style.fontSize = binding.value.tamanhofonte
     
     let totalCaracteres = 20
-    if(biding.value?.totalCaracteres) totalCaracteres = biding.value.totalCaracteres
+    if(binding.value?.totalCaracteres) totalCaracteres = binding.value.totalCaracteres
 
     let textoOriginal = el.innerText
     let tamanhoTextoOriginal = textoOriginal.length
@@ -25,5 +25,18 @@ Vue.directive('texto', {
     el.innerText = textoAjustado
   }
 }) 
+
+Vue.directive('posicao', {
+    created(el, binding) {
+        console.log(el, binding.arg, binding.value)
+
+        const posicoesPossiveis = ['relative', 'fixed', 'absolute']
+
+        if(posicoesPossiveis.includes(binding.arg)) {
+            el.style.position = binding.arg
+            el.style.top = `${binding.value}px`
+        }
+    }
+})
 
 Vue.mount('#app')
